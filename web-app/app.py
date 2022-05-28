2ff5ddec0d365e94769eb1b994e0f9ca941d4423
Sasha Tsepilova <oleksandra.tsepilova@ucu.edu.ua>
2022-05-28 15:57:28 +0300

    web app logic done

--- web-app/app.py
+++ web-app/app.py
@@ -1,23 +1,47 @@
+from unittest import result
 from flask import Flask, render_template, request, url_for, send_file
+from matplotlib.style import use
+from scripts.calc_recommendation import recommend
+import requests
+
 
 app = Flask(__name__)
 
 chosen_films=[]
+user_ratings={}
+BASE_URL = 'https://api.themoviedb.org/3/movie/'
+API_KEY ='?api_key=523e924d1ef2f06415de79b602cc54d6'
+BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500'
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     button_available = False
     if request.method == 'POST':
         movie_name = request.form.get("movie")
-        grade = request.form.get("grade")
-        chosen_films.append(movie_name+" with grade "+grade)
+        grade = float(request.form.get("grade"))
+        if(not movie_name in user_ratings):
+            chosen_films.append(movie_name+" with grade "+str(grade))
+            user_ratings[movie_name] = grade
     if len(chosen_films) >= 10:
         button_available = True
     return render_template("index.html", content=chosen_films, button_av = button_available)
 
 @app.route('/recommendation', methods = ['GET', 'POST'])
 def recommendation():
-    pass
+
+    global user_ratings
+    result = recommend(user_ratings)
+    for i in range(len(result)):
+        
+        response = requests.request(
+            "GET", BASE_URL+str(result[i][1])+API_KEY)
+        if(response.status_code == 200):
+            result[i][1] = BASE_IMG_URL + response.json()["poster_path"]
+        else:
+            result[i][1] ="https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg"
+    
+    user_ratings = {}
+    return render_template("recommendation.html", content=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- web-app/scripts/calc_recommendation.py
+++ web-app/scripts/calc_recommendation.py
@@ -1,5 +1,42 @@
 import numpy as np
 import pandas as pd
 
-def recommend(iters:int, reg:float, rate:float, ):
-    pass
+DF_FILMS = pd.read_csv('ml-25m/movies.csv')
+N_ITER = 100
+REGULARIZATION = 0.02
+LEARNING_RATE = 0.005
+
+def recommend(user_ratings: dict):
+    df_items = pd.read_csv('obtained_data/items_factors.csv', header=None)
+    items = df_items.to_numpy()
+    df_bias = pd.read_csv('obtained_data/items_bias.csv', header=None)
+    bias = df_bias.to_numpy()
+
+    user_factors = np.random.normal(0, 1, (10))
+    for _ in range(N_ITER):
+        for id, rating in user_ratings.items():
+            error = rating - user_factors @ items[int(id)]
+            user_factors = user_factors + LEARNING_RATE*(error*items[int(id)] - REGULARIZATION*user_factors)
+    all_ratings = items @ user_factors
+    for idx, elem in enumerate(all_ratings):
+        elem += bias[idx]
+    all_ratings = sorted(enumerate(all_ratings), key=lambda i: i[1], reverse=True)
+    right = 0
+    recommendations = []
+    for rating in all_ratings:
+        if str(rating[0]) not in user_ratings.keys():
+            recommendations.append(rating[0])
+            right += 1
+        if right == 5:
+            break
+    
+    df2 = DF_FILMS[DF_FILMS['innerId'].isin(recommendations)]
+    df2=df2[['title', 'tmdbId']]
+    return df2.values.tolist()
+
+
+if __name__ == '__main__': 
+    user_ratings = {'1824' : 5, '12682': 5, '0': 4, '14253': 4.5,
+    '9763' : 5, '817': 3, '2665': 4.5, '371': 4, '2427': 5, '29': 5} #inner_id : rating
+
+    print(recommend(user_ratings))
